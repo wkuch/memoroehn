1ae5ededa2c9370dd570764fa516c5abf893c527
wkuch <wanjakuch@gmail.com>
2022-03-26 15:24:39 +0100

    remove dark bg

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Board from "./components/board/Board";
 
 function App() {
   return (
-    <div className="container has-background-dark">
+    <div className="container">
       <div className="m-6">
         <Board />
       </div>
